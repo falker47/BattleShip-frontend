19d09cb07f70de0f53e293e9a2fc2f597b5cd59b
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2023-04-19 14:55:59 +0200

    list of players

--- BattleshipFrontend/src/app/landing-page/landing-page.component.ts
+++ BattleshipFrontend/src/app/landing-page/landing-page.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
+  // Temporary memory --> backend
+  players: string[] = ["alexia", "maurizio", "artiom", "flavio", "daniele"];
+  limitNumPlayers: number = 6;
+
+
+  addPlayer(name: string) {
+    console.log(name);
+    if (!name) return;
+
+    if (this.players.length < this.limitNumPlayers) {
+      this.players.push(name);
+      name = '';
+    }
+  }
+  
 }
