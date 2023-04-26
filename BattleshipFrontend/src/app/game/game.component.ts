4ef0aae358a50d7d50cef9dd3b8d2c7269188b50
Flavio Gioia <flaviog2@hotmail.it>
2023-04-26 21:57:14 +0200

    leaderboard completed

--- BattleshipFrontend/src/app/game/game.component.ts
+++ BattleshipFrontend/src/app/game/game.component.ts
@@ -1,7 +1,13 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PlayerService } from '../api/player.service';
-import { PlayerFrontendGame, CellApi, GridApi, PlayerApi, Shot } from '../api/models';
+import {
+  PlayerFrontendGame,
+  CellApi,
+  GridApi,
+  PlayerApi,
+  Shot,
+} from '../api/models';
 
 window.addEventListener('beforeunload', (event) => {
   event.returnValue = `Are you sure you want to leave?`;
@@ -26,52 +32,48 @@ export class GameComponent implements OnInit {
   public userGrid!: GridApi;
   public shotGrid!: GridApi;
 
-
   constructor(private router: Router, private playerService: PlayerService) {
     this.players = this.playerService.getGamePlayers();
     this.userGrid = this.playerService.getUserGrid();
     this.shotGrid = this.playerService.getShotGrid();
   }
 
-
   ngOnInit() {
     this.playerBoard = this.getEmptyBoard();
     this.preparePlayers();
     this.currentPlayer = this.getCurrentPlayer(0);
-
-    this.players.forEach((player) => {
-      this.playersLeaderboard.push(player);
-    });
   }
-
 
   getCellState(x: number, y: number, type: string): number {
     let cells: CellApi[] = [];
     if (type === 'user') {
-      this.userGrid.Cells.forEach(cells1 => {
-        cells1.forEach(cell => {
+      this.userGrid.Cells.forEach((cells1) => {
+        cells1.forEach((cell) => {
           cells.push(cell);
-        })
-      })
-  
-      let foundCell = cells.find(cell => x === cell.Xaxis && y === cell.Yaxis);
+        });
+      });
+
+      let foundCell = cells.find(
+        (cell) => x === cell.Xaxis && y === cell.Yaxis
+      );
       return Number(foundCell?.State);
     }
 
     if (type === 'shot') {
-      this.shotGrid.Cells.forEach(cells1 => {
-        cells1.forEach(cell => {
+      this.shotGrid.Cells.forEach((cells1) => {
+        cells1.forEach((cell) => {
           cells.push(cell);
-        })
-      })
-  
-      let foundCell = cells.find(cell => x === cell.Xaxis && y === cell.Yaxis);
+        });
+      });
+
+      let foundCell = cells.find(
+        (cell) => x === cell.Xaxis && y === cell.Yaxis
+      );
       return Number(foundCell?.State);
     }
 
     return 0;
   }
-
 
   private getEmptyBoard(): number[][] {
     for (let i = 0; i <= this.players.length; i++) {
@@ -80,11 +82,9 @@ export class GameComponent implements OnInit {
     return Array.from({ length: this.width }, () => Array(this.width).fill(0));
   }
 
-
   getCurrentPlayer(i: number): PlayerFrontendGame {
     return this.playersData[i];
   }
-
 
   preparePlayers() {
     this.players.forEach((player) => {
@@ -97,113 +97,117 @@ export class GameComponent implements OnInit {
     });
   }
 
-
   toggleIsReady() {
     this.isReady = !this.isReady;
   }
 
-
   async getNextPlayer() {
     let index: number = ++this.currentIndex;
-    console.log('current index: GET NEXT PLAYER ' + this.currentIndex)
-    console.log('index: (has to be equal to current index!) ' + index)
-    
+
     if (this.playersData.length === index) {
       this.currentIndex = 0;
       this.currentPlayer = this.playersData[this.currentIndex];
-      console.log('this.currentIndex = 0' + this.currentIndex)
-    }
-    else {
+    } else {
       this.currentPlayer = this.playersData[index];
-      console.log('this.currentIndex !== 0' + this.currentIndex)
     }
     this.shot.pop();
     this.toggleIsReady();
-    
-    await this.playerService.getGridByPlayerId(this.currentPlayer.id, this.width, true).toPromise().then(res => {
-      if (res) {
-        this.playerService.setUserGrid(res);
-        this.userGrid = this.playerService.getUserGrid();
-      }
-    });
-    await this.playerService.getGridByPlayerId(this.currentPlayer.id, this.width, false).toPromise().then(res => {
-      if (res) {
-        this.playerService.setShotGrid(res);
-        this.shotGrid = this.playerService.getShotGrid();
-      }
-    });
+
+    await this.playerService
+      .getGridByPlayerId(this.currentPlayer.id, this.width, true)
+      .toPromise()
+      .then((res) => {
+        if (res) {
+          this.playerService.setUserGrid(res);
+          this.userGrid = this.playerService.getUserGrid();
+        }
+      });
+    await this.playerService
+      .getGridByPlayerId(this.currentPlayer.id, this.width, false)
+      .toPromise()
+      .then((res) => {
+        if (res) {
+          this.playerService.setShotGrid(res);
+          this.shotGrid = this.playerService.getShotGrid();
+        }
+      });
+    console.log(this.playersData + 'dopo');
   }
 
-
-  async saveShot (x: number, y: number) {
+  async saveShot(x: number, y: number) {
     if (this.shot.length === 0) {
       const playerShot: Shot = {
         id: this.currentPlayer.id,
         xAxis: x,
         yAxis: y,
       };
-    this.shot.push(playerShot);
-      
-      await this.playerService.postShot(playerShot).toPromise().then(res => {
-        if (res) {
-          let logArray = res.log.split(';');
-          logArray.forEach(log => {
-            if (log !== "") {
-              this.logs.unshift(log);
-            }
-          });
-        }})
-      }
+      this.shot.push(playerShot);
 
-      this.playerService.getPlayers().toPromise().then(res => {
+      await this.playerService
+        .postShot(playerShot)
+        .toPromise()
+        .then((res) => {
+          if (res) {
+            let logArray = res.log.split(';');
+            logArray.forEach((log) => {
+              if (log !== '') {
+                this.logs.unshift(log);
+              }
+            });
+          }
+        });
+    }
+
+    this.playerService
+      .getPlayers()
+      .toPromise()
+      .then((res) => {
         if (res) {
           this.playerService.setGamePlayers(res);
-          this.playersLeaderboard = this.playerService.getGamePlayers();
+          this.players = this.playerService.getGamePlayers();
         }
       });
 
-    this.isPlayerAlive();
+    this.isPlayersAlive();
     this.isGameOver();
   }
 
-
   isGameOver(): boolean {
-    const team0Alive = this.playersData.find(player => player.team === 0);
-    const team1Alive = this.playersData.find(player => player.team === 1);
-    
+    const team0Alive = this.playersData.find((player) => player.team === 0);
+    const team1Alive = this.playersData.find((player) => player.team === 1);
+
     if (!team0Alive || !team1Alive) return true;
     else return false;
   }
 
-
-  isPlayerAlive() {
-    let shipsHP: number[] = [];
-    let index: number = this.currentIndex + 1;
-    console.log('this.currentIndex IS PLAYER ALIVE: ' + this.currentIndex)
-    let newIndex = this.players.length === index ? 0 : index;
-    this.playerService.getShipsByPlayerId(this.playersData[newIndex].id).toPromise().then(res => {
-      if (res) {
-        res.forEach(ship => {
-          if (ship.hp !== 0) {
-            shipsHP.push(ship.hp)
+  isPlayersAlive() {
+    this.playersData.forEach((playerPlaying, i) => {
+      let shipsHP: number[] = [];
+      this.playerService
+        .getShipsByPlayerId(playerPlaying.id)
+        .toPromise()
+        .then((res) => {
+          if (res) {
+            res.forEach((ship) => {
+              if (ship.hp !== 0) {
+                shipsHP.push(ship.hp);
+              }
+            });
           }
-        })
-      }
+          if (shipsHP.length === 8) {
+            this.playersData.splice(i, 1);
+          }
+        });
     });
-    if (shipsHP.length === 0) {
-      this.playersData.slice(this.currentIndex, 1);
-    } 
   }
-
 
   sortLeaderboard(): PlayerApi[] {
-    return this.playersLeaderboard.sort((a, b) => b.points - a.points);
+    return this.players.sort((a, b) => b.points - a.points);
   }
 
-
   sortFinalLeaderboard(): PlayerApi[] {
-    const team0Alive = this.playersData.find(player => player.team === 0);
-    const team1Alive = this.playersData.find(player => player.team === 1);
+    const team0Alive = this.playersData.find((player) => player.team === 0);
+    const team1Alive = this.playersData.find((player) => player.team === 1);
     let playersByPoints = this.players.sort((a, b) => b.points - a.points);
 
     if (team0Alive && !team1Alive) {
@@ -211,13 +215,10 @@ export class GameComponent implements OnInit {
     }
     if (team1Alive && !team0Alive) {
       return playersByPoints.sort((a, b) => b.team - a.team);
-    }
-    else return playersByPoints;
+    } else return playersByPoints;
   }
-
 
   restartGame() {
     this.router.navigate(['/']);
   }
-
 }
