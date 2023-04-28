beb66b1834fdd47de53754edc9c9abd9fc6e0017
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2023-04-28 12:43:01 +0200

    improved random team selection and more input added more input verifications

--- BattleshipFrontend/src/app/landing-page/landing-page.component.ts
+++ BattleshipFrontend/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PlayerApi, PlayerInitialData } from '../api/models';
+import { PlayerInitialData } from '../api/models';
 import { Router } from '@angular/router';
 import { PlayerService } from '../api/player.service';
 
@@ -15,24 +15,26 @@ window.addEventListener('beforeunload', (event) => {
 export class LandingPageComponent {
   public players: PlayerInitialData[] = [];
   public limitNumPlayers = 6;
-  public gamePlayers: PlayerApi[] = [];
 
   constructor(private router: Router, private playerService: PlayerService) {}
 
-  addPlayer(name: string) {
-    if (!name) return;
-
+  public addPlayer(inputName: string) {
+    const name = inputName.replace(/\s/g, '');
+    if (!inputName || name === "" || this.isNameTaken(name)) {
+      return;
+    }
+    
     if (this.players.length === 0) {
       this.players.push({
         name: name,
-        team: 0,
+        team: Math.floor(Math.random() * 2),
       });
       return;
     }
     if (this.players.length === 1) {
       this.players.push({
         name: name,
-        team: 1,
+        team: this.players[0].team === 0 ? 1 : 0,
       });
       return;
     }
@@ -44,11 +46,23 @@ export class LandingPageComponent {
     }
   }
 
-  removePlayer(player: PlayerInitialData) {
+  private isNameTaken(name: string): boolean {
+    let repeatedNames: boolean[] = [];
+    this.players.forEach(player => {
+      if (player.name === name) repeatedNames.push(true);
+      else repeatedNames.push(false);
+    })
+
+    if (repeatedNames.find(item => item === true)) return true;
+    else return false;
+  }
+
+  public removePlayer(player: PlayerInitialData) {
     this.players.splice(this.players.indexOf(player), 1);
   }
 
-  async confirmPlayers() {
+  public async confirmPlayers() {
+    console.log(this.players)
     await this.playerService.postCreateGame(this.players).toPromise().then(async (res) => {
       if (res) {
         await this.playerService.getPlayers().toPromise().then(res => {
